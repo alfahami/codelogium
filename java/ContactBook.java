986dc65f06745e238e300fb0246a9c01fe4c8dcc
Al-Fahami Toihir <altoihir@gmail.com>
2026-05-18 20:23:04 +0100

    feat: add Contact pojo class

---
+++ java/ContactBook.java
@@ -0,0 +1,26 @@
+public class ContactBook {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    class Contact {
+        private String name;
+        private String phone;
+        private String email;
+
+        public Contact(String name, String phone, String email) {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        public String getName()  { return this.name;  }
+        public String getPhone() { return this.phone; }
+        public String getEmail() {return this.email;  }
+
+        public void setName(String name)   { this.name = name;   }
+        public void setPhone(String phone) { this.phone = phone; }
+        public void setEmail(String email) { this.email = email; }
+    }
+}
