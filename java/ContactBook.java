a263a9bbd25b9e1a642f1254c9dd340bfd394040
Al-Fahami Toihir <altoihir@gmail.com>
2026-05-23 21:52:27 +0100

    fix: use hashMap as it is the best fit for the problem instead of List

--- java/ContactBook.java
+++ java/ContactBook.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class ContactBook {
 
     public static void main(String[] args) {
-        List<Contact> contacts = new ArrayList<>();
+
+        HashMap<String, Contact> contacts = new HashMap<>();
 
         Contact c1 = new Contact("Tupac", "0627528394", "tupac.shakur@gmail.com");
         Contact c2 = new Contact("Tupac", "0627528394", "tupac.shakur@gmail.com");
@@ -15,13 +15,15 @@ public class ContactBook {
         listAllContacts(contacts);
     }
 
-    private static boolean addContact(Contact contact, List<Contact> contacts) {
-        return contacts.add(contact);
+    private static Contact addContact(Contact contact, HashMap<String, Contact> contacts) {
+        if(contacts.put(contact.getName(), contact) == null) 
+            return contact;
+         else return null;
     }
 
-    private static void listAllContacts(List<Contact> contacts) {
-        for (Contact contact : contacts) {
-            System.out.println(contact.toString());
+    private static void listAllContacts(HashMap<String, Contact> contacts) {
+        for (String key : contacts.keySet()) {
+            System.out.println(contacts.get(key).toString());
         }
     }
 
@@ -43,5 +45,9 @@ public class ContactBook {
         public void setName(String name)   { this.name = name;   }
         public void setPhone(String phone) { this.phone = phone; }
         public void setEmail(String email) { this.email = email; }
+
+        public String toString() {
+            return "Contact Info :" + " Name :" + name + " | " + "Phone: " + phone + " | " + "Email: " + email + " |";
+        }
     }
 }
